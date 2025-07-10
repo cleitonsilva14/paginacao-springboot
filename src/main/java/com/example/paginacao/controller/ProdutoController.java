3cf74d08974e37a6bbbeaa5ddfdf98505157884c
cleitonsilva14 <cleiton18ads@gmail.com>
2025-07-10 19:34:20 -0300

    Criando outro método para paginação

--- src/main/java/com/example/paginacao/controller/ProdutoController.java
+++ src/main/java/com/example/paginacao/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,6 +38,13 @@ public class ProdutoController {
         Page<Produto> produtos = produtoRepository.findAll(PageRequest.of(page, size, Sort.by(sortDirection, orderBy)));
 
         return produtos.getContent();
+    }
+
+    @GetMapping("/paginado")
+    public ResponseEntity<Page<Produto>> findAll(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(produtoRepository.findAll(pageable));
     }
 
 
